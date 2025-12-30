431ab590441896326c6e7c4c3528e02af1b54333
디옹_김동규 <ynwa@sooplive.com>
2025-12-30 11:47:51 +0900

    feat: Noto Sans KR 폰트 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,15 @@
 import "./globals.css";
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_KR } from "next/font/google";
 import { Analytics } from "./analytics";
 import { Header } from "./header";
 import { Footer } from "./footer";
 import { doge } from "./doge";
 
-const geist = Geist({
+const notoSansKr = Noto_Sans_KR({
   subsets: ["latin"],
-  variable: "--font-geist-sans"
+  weight: ["400", "500", "600", "700"],
 });
-
-const geistMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-geist-mono",
-  fallback: ["ui-monospace", "SFMono-Regular", "Consolas", "Liberation Mono", "Menlo", "monospace"]
-});
-
-export const metadata = {
-  title: "Guillermo Rauch's blog",
-  description:
-    "Guillermo Rauch is the CEO and founder of Vercel, a software engineer, and the creator of Next.js, Mongoose, Socket.io and other open source libraries.",
-  openGraph: {
-    title: "Guillermo Rauchg's blog",
-    description:
-      "Guillermo Rauch is the CEO and founder of Vercel, a software engineer, and the creator of Next.js, Mongoose, Socket.io and other open source libraries.",
-    url: "https://rauchg.com",
-    siteName: "Guillermo Rauchg's blog",
-    images: ["/opengraph-image"],
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@rauchg",
-    creator: "@rauchg",
-  },
-  metadataBase: new URL("https://rauchg.com"),
-};
 
 export const viewport = {
   themeColor: "transparent",
@@ -49,7 +23,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geist.variable} ${geistMono.variable} ${geist.className} antialiased`}
+      className={`${notoSansKr.className} antialiased`}
       suppressHydrationWarning={true}
     >
       <head>
